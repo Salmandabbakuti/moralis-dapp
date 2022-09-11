306dafe5c6c3d48428227420a07c85d588bee395
Salman Dabbakuti <dabbakutisalman@gmail.com>
2022-09-11 23:51:14 +0530

    updated: setup moralis authentication

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useMoralis } from "react-moralis";
 import "./styles.css";
 
 export default function App() {
-  const { authenticate, isAuthenticated, user } = useMoralis();
+  const { authenticate, logout, isAuthenticated, user } = useMoralis();
   return (
     <div className="App">
       {!isAuthenticated ? (
-        <button onClick={() => authenticate()}>Authenticate</button>
+        <button onClick={() => authenticate({ signingMessage: "Welcome to Moralis Login With Ethereum" })}>Authenticate</button>
       ) : (
-        <h1>Welcome {user.get("username")}</h1>
+        <>
+          <h1>Welcome, {user.get("ethAddress")}</h1>
+          <button onClick={logout}>Signout</button>
+        </>
       )}
     </div>
   );
